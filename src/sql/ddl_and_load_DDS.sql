c34de413d9d47ba6e3f7a933a7cbcec4c2c53d72
vfx-beavers <63024074+vfx-beavers@users.noreply.github.com>
2023-09-05 19:01:41 +0300

    ver 2

--- src/sql/ddl_and_load_DDS.sql
+++ src/sql/ddl_and_load_DDS.sql
@@ -1,13 +1,12 @@
-drop table if exists STV2023081241__DWH.h_users CASCADE;
-drop table if exists STV2023081241__DWH.h_groups CASCADE;
-drop table if exists STV2023081241__DWH.l_user_group_activity CASCADE;
-drop table if exists STV2023081241__DWH.s_auth_history CASCADE;
-
+DELETE from STV2023081241__DWH.h_users;
+DELETE from STV2023081241__DWH.h_groups;
+DELETE from STV2023081241__DWH.l_user_group_activity;
+DELETE from STV2023081241__DWH.s_auth_history;
 
 ----- пользователи
 
 
-create table STV2023081241__DWH.h_users
+create table IF NOT EXISTS STV2023081241__DWH.h_users
 (
     hk_user_id bigint primary key,
     user_id      int,
@@ -33,7 +32,7 @@ where hash(user_id) not in (select hk_user_id from STV2023081241__DWH.h_users);
 ------- группы
 
 
-create table STV2023081241__DWH.h_groups
+create table IF NOT EXISTS STV2023081241__DWH.h_groups
 (
     hk_group_id       bigint primary key,
     group_id          int,
@@ -51,7 +50,7 @@ INSERT INTO STV2023081241__DWH.h_groups(hk_group_id, group_id, registration_dt, 
 select distinct 
        hash(group_id) as hk_group_id,
        group_id,
-       datetime, 
+       "datetime", 
        now() as load_dt,
        's3' as load_src
        from STV2023081241__STAGING.group_log
@@ -62,7 +61,7 @@ and event = 'create';
 ----- пользовательская активность линк
 
 
-create table STV2023081241__DWH.l_user_group_activity
+create table IF NOT EXISTS STV2023081241__DWH.l_user_group_activity
 (
 	hk_l_user_group_activity bigint primary key,
 	hk_user_id bigint not null CONSTRAINT fk_h_users REFERENCES STV2023081241__DWH.h_users (hk_user_id),
@@ -71,7 +70,7 @@ create table STV2023081241__DWH.l_user_group_activity
 	load_src          varchar(20)
 )
 order by load_dt
-SEGMENTED BY hk_l_user_group_activity all nodes
+SEGMENTED BY hk_user_id all nodes
 PARTITION BY load_dt::date
 GROUP BY calendar_hierarchy_day(load_dt::date, 3, 2);
 
@@ -92,25 +91,23 @@ where hash(hu.hk_user_id, hg.hk_group_id) not in (select hk_l_user_group_activit
 ----- история авторизаций сат
 
 
-create table STV2023081241__DWH.s_auth_history
+create table IF NOT EXISTS STV2023081241__DWH.s_auth_history
 (hk_l_user_group_activity bigint not null CONSTRAINT fk_l_user_group_activity REFERENCES STV2023081241__DWH.l_user_group_activity (hk_l_user_group_activity)
 , user_id_from integer, event varchar(100), event_dt datetime, load_dt datetime, load_scr varchar(20))
-order by load_dt
+order by event_dt
 SEGMENTED BY hk_l_user_group_activity all nodes
-PARTITION BY load_dt::date
-GROUP BY calendar_hierarchy_day(load_dt::date, 3, 2);
+PARTITION BY event_dt::date
+GROUP BY calendar_hierarchy_day(event_dt::date, 3, 2);
 
 INSERT INTO STV2023081241__DWH.s_auth_history (hk_l_user_group_activity, user_id_from, event, event_dt, load_dt, load_scr)
 select distinct   
 	luga.hk_l_user_group_activity
 	, gl.user_id_from
 	, gl.event
-	, gl.datetime 
+	, gl."datetime" 
 	, now() as load_dt
 	,'s3' as load_src
 from STV2023081241__STAGING.group_log as gl
 left join STV2023081241__DWH.h_groups as hg on gl.group_id = hg.group_id
 left join STV2023081241__DWH.h_users as hu on gl.user_id = hu.user_id
-left join STV2023081241__DWH.l_user_group_activity as luga on (hg.hk_group_id = luga.hk_group_id) and (hu.hk_user_id = luga.hk_user_id)
-where luga.hk_l_user_group_activity not in (select hk_l_user_group_activity from STV2023081241__DWH.s_auth_history)
-and gl.event = 'add' or gl.event = 'leave';
+left join STV2023081241__DWH.l_user_group_activity as luga on (hg.hk_group_id = luga.hk_group_id) and (hu.hk_user_id = luga.hk_user_id);
--- src/sql/ddl_and_load_STG.sql
+++ src/sql/ddl_and_load_STG.sql
@@ -1,16 +1,16 @@
-DROP TABLE IF EXISTS STV2023081241__STAGING.group_log;
+DELETE from STV2023081241__STAGING.group_log;
 DROP TABLE IF EXISTS STV2023081241__STAGING.group_log_rej;
 
-CREATE TABLE STV2023081241__STAGING.group_log
+CREATE TABLE IF NOT EXISTS STV2023081241__STAGING.group_log
 (
     group_id INT NOT NULL,
     user_id INT,
     user_id_from INT,
     event varchar(50),
-    datetime timestamp
+    "datetime" timestamp
 );
 
-COPY STV2023081241__STAGING.group_log (group_id, user_id, user_id_from, event, datetime)
+COPY STV2023081241__STAGING.group_log (group_id, user_id, user_id_from, event, "datetime")
 FROM LOCAL 'E:\\Data\\Lab\\S6 Аналитические базы данных\\data\\group_log.csv'
 DELIMITER ','
 REJECTED DATA AS TABLE STV2023081241__STAGING.group_log_rej;
